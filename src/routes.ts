cac0bcd2b7c22914f557e02cb34f64f4a4f5fbee
Ranieri Lucas <80073336+ranieri3232@users.noreply.github.com>
2021-10-25 13:33:49 -0300

    second deploy on heroku

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,25 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable import/no-unresolved */
-import { Router } from 'express';
-import { AuthenticateUserController } from './controllers/AuthenticateUserController';
-import { CreateMessageController } from './controllers/CreateMessageController';
-import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
-import { ProfileUserController } from './controllers/ProfileUserController';
-import { ensureAuthenticated } from './middleware/ensureAuthenticated';
+import { Router, Request, Response } from 'express';
+// import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+// import { CreateMessageController } from './controllers/CreateMessageController';
+// import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
+// import { ProfileUserController } from './controllers/ProfileUserController';
+// import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 const router = Router();
 
-router.post('/authenticate', new AuthenticateUserController().handle);
+// router.post('/authenticate', new AuthenticateUserController().handle);
 
-router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
+// router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
 
-router.get('/messages/last3', new GetLast3MessagesController().handle);
+// router.get('/messages/last3', new GetLast3MessagesController().handle);
 
-router.get('/profile', ensureAuthenticated, new ProfileUserController().handle);
+// router.get('/profile', ensureAuthenticated, new ProfileUserController().handle);
+
+router.get('/hello', (req: Request, res: Response) => {
+  res.json({ hello: 'world' });
+});
 
 export { router };
